dd3c260a09aa49543b6c67b15707562d2bf9f26d
glovic <xtineviktor@gmail.com>
2024-09-30 14:20:23 +0100

    feat: add user creation endpoint with validation for email and password

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,4 @@
 import { Router } from 'express';
-
 // Import necessary controllers
 import AppController from '../controllers/AppController';
 // Import UsersController
@@ -9,9 +8,10 @@ import UsersController from '../controllers/UsersController.js';
 const router = Router();
 
 router.get('/status', AppController.getStatus); // Health check for Redis and DB
+
 router.get('/stats', AppController.getStats); // Get user and file statistics
+
 router.post('/users', UsersController.postNew); // Create a new user
 
-// Export the router
-export default router;
+export default router; // Export the router
 
