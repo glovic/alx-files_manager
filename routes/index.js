01805e54517761e9b0dc96c73a176c53f367b400
glovic <xtineviktor@gmail.com>
2024-09-30 16:16:09 +0100

    feat: implement authentication endpoints for user sign-in and sign-out

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,8 @@ import { Router } from 'express';
 import AppController from '../controllers/AppController';
 // Import UsersController
 import UsersController from '../controllers/UsersController.js';
+// Import AuthController
+import AuthController from '../controllers/AuthController.js';
 
 // Define API routes
 const router = Router();
@@ -11,6 +13,8 @@ const router = Router();
 router.get('/status', AppController.getStatus); // Health check for Redis and DB
 router.get('/stats', AppController.getStats); // Get user and file statistics
 router.post('/users', UsersController.postNew); // Create a new user
+router.get('/connect', AuthController.getConnect); // User login
+router.get('/disconnect', AuthController.getDisconnect); // User logout
+router.get('/users/me', UsersController.getMe); // Retrieve current user information
 
-// Export the router
-export default router;
+export default router; // Export the router
